15ece76cf1c98f7c034283baad8c9c863c4ec9ef
Vivek Sant <vsant@hcs.harvard.edu>
2011-04-11 10:58:52 -0400

    Update to v1.7

--- Shuttleboy.wdgt/scripts.js
+++ Shuttleboy.wdgt/scripts.js
@@ -2,7 +2,7 @@
    Vivek Sant
    vsant@hcs.harvard.edu */
 
-verCurr = "1.6";
+verCurr = "1.7";
 
 function toGar(arr)   { prettyPrint("d1", "to Garden",     arr); }
 function toMem(arr)   { prettyPrint("d2", "to MemHall",    arr); }
